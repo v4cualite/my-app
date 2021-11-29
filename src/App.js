ab45ba56cd3ae85b71a65aba7dd50298703576f9
Sergei Tsoi <v4cualite@gmail.com>
2021-11-29 21:56:36 +0300

    name changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
-    const myName = "Sergei"
+    const myName = "Sergej"
     return (
         <div className="App">
             <header className="App-header">
