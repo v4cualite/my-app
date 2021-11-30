7694a2dd07272bbfc00fcc58fbb89c1fe9f2fdab
Sergei Tsoi <v4cualite@gmail.com>
2021-11-30 18:15:09 +0300

    use tailwincss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import "tailwindcss/tailwind.css"
 import logo from "./logo.svg"
 import "./App.css"
-
 function App() {
     const myName = "Sergej"
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello, {myName}.</p>
+            <header className="flex mh-100 flex-col bg-blue-400 items-center justify-center fs-calc(10px+2vmin) color-white">
+                <img src={logo} className="h-40 animate-spin " alt="logo" />
+                <p className="text-red-400"> Hello, {myName}.</p>
                 <a
-                    className="App-link"
+                    className="text-black-900"
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
