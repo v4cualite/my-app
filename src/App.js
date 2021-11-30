da7b32d7b316b77110745656c14cb9f9f4900004
Sergei Tsoi <v4cualite@gmail.com>
2021-11-30 19:23:31 +0300

    change styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "tailwindcss/tailwind.css"
 import logo from "./logo.svg"
-import "./App.css"
+
 function App() {
     const myName = "Sergej"
     return (
         <div className="App">
-            <header className="flex mh-100 flex-col bg-blue-400 items-center justify-center fz-calc(10px + 2vmin) color-white">
+            <header className="flex min-h-screen flex-col bg-blue-400 items-center justify-center text-4xl">
                 <img src={logo} className="h-40 animate-spin " alt="logo" />
                 <p className="text-red-400"> Hello, {myName}.</p>
                 <a
@@ -14,7 +14,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+                    Push The Button
                 </a>
             </header>
         </div>
