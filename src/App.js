b76d14b006bc315bd558b25a1890ac3ec3a00413
Sergei Tsoi <v4cualite@gmail.com>
2021-12-01 15:05:02 +0300

    add Header.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import "tailwindcss/tailwind.css"
-import logo from "./logo.svg"
-
+import Header from "./Header"
 function App() {
-    const myName = "Sergej"
     return (
         <div className="App">
-            <header className="flex min-h-screen flex-col bg-blue-400 items-center justify-center text-4xl">
-                <img src={logo} className="h-40 animate-spin " alt="logo" />
-                <p className="text-red-400"> Hello, {myName}.</p>
-                <a
-                    className="text-black-900"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Push The Button
-                </a>
-            </header>
+            <Header myName="Sergej" />
         </div>
     )
 }
-
 export default App
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import logo from "./logo.svg"
+function Header(props) {
+    return (
+        <header className="flex min-h-screen flex-col bg-blue-400 items-center justify-center text-4xl">
+            <img src={logo} className="h-40 animate-spin " alt="logo" />
+            <p className="text-red-400"> Hello, {props.myName || "world"}.</p>
+            <a
+                className="text-black-900"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Push The Button
+            </a>
+        </header>
+    )
+}
+export default Header
