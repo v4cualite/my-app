8af1569a5ce5dd8229a8ce47daa264116e8e2007
Sergei Tsoi <v4cualite@gmail.com>
2021-12-02 15:30:45 +0300

    add useState

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import logo from "./logo.svg"
+import React, { useState } from "react"
+
 function Header(props) {
+    const [myName, setMyName] = useState(props.myName)
     return (
         <header className="flex min-h-screen flex-col bg-blue-400 items-center justify-center text-4xl">
             <img src={logo} className="h-40 animate-spin " alt="logo" />
