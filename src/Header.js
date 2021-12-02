07924528a97982caec59d7662e47e5bdfdb6ea42
Sergei Tsoi <v4cualite@gmail.com>
2021-12-02 21:44:07 +0300

    add surname

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,7 @@ import React, { useState } from "react"
 
 function Header(props) {
     const [myName, setMyName] = useState(props.myName)
+    const [surName, setSurName] = useState("")
     return (
         <header className="flex min-h-screen flex-col bg-blue-400 items-center justify-center text-4xl">
             <img src={logo} className="h-40 animate-spin " alt="logo" />
@@ -13,7 +14,15 @@ function Header(props) {
                 value={myName}
                 onChange={(e) => setMyName(e.target.value)}
                 className="text-red-400 text-center"
-            ></input>
+            />
+
+            <input
+                type="text"
+                value={surName}
+                onChange={(e) => setSurName(e.target.value)}
+                className="  start text-red-400 text-center mt-6"
+            />
+
             <p className="text-red-400"> Hello, {myName || "world"}.</p>
             <a
                 className="text-black-900"
