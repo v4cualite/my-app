0e5e5e1cc7027b69de730bc0928a4cca3327f601
Sergei Tsoi <v4cualite@gmail.com>
2021-12-02 22:19:27 +0300

    add flex

--- src/Header.js
+++ src/Header.js
@@ -9,21 +9,30 @@ function Header(props) {
     return (
         <header className="flex min-h-screen flex-col bg-blue-400 items-center justify-center text-4xl">
             <img src={logo} className="h-40 animate-spin " alt="logo" />
-            <input
-                type="text"
-                value={myName}
-                onChange={(e) => setMyName(e.target.value)}
-                className="text-red-400 text-center"
-            />
-
-            <input
-                type="text"
-                value={surName}
-                onChange={(e) => setSurName(e.target.value)}
-                className="  start text-red-400 text-center mt-6"
-            />
-
-            <p className="text-red-400"> Hello, {myName || "world"}.</p>
+            <div className="flex flex-col justify-between ">
+                <label className="flex justify-end">
+                    Имя:
+                    <input
+                        type="text"
+                        value={myName}
+                        onChange={(e) => setMyName(e.target.value)}
+                        className="text-red-400 text-center"
+                    />
+                </label>
+                <label>
+                    Фамилия:
+                    <input
+                        type="text"
+                        value={surName}
+                        onChange={(e) => setSurName(e.target.value)}
+                        className="  start text-red-400 text-center mt-6"
+                    />
+                </label>
+            </div>
+            <p className="text-red-400">
+                {" "}
+                Hello, {myName + surName || "world"}.
+            </p>
             <a
                 className="text-black-900"
                 href="https://reactjs.org"
